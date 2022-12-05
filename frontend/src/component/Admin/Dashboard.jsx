e2a01a4e8b221e3d14e69e251666946aefbeb63e
Ignacio sanchez <56764826+nachaxed@users.noreply.github.com>
2022-12-05 12:07:34 -0300

    new dashboard

--- frontend/src/component/Admin/Dashboard.jsx
+++ frontend/src/component/Admin/Dashboard.jsx
@@ -44,75 +44,68 @@ const Dashboard = () => {
           totalAmount += item.totalPrice;
         });
 
-    const lineState = {
-        labels: ["Initial Amount", "Amount Earned"],
-        datasets: [
-          {
-            label: "TOTAL AMOUNT",
-            backgroundColor: ["#3BB77E"],
-            hoverBackgroundColor: ["#3BB77E"],
-            data: [0, totalAmount],
-          },
-        ],
+        const lineState = {
+          labels: ["Initial Amount", "Amount Earned"],
+          datasets: [
+            {
+              label: "TOTAL AMOUNT",
+              backgroundColor: ["tomato"],
+              hoverBackgroundColor: ["rgb(197, 72, 49)"],
+              data: [0, totalAmount],
+            },
+          ],
+        };
+      
+        const doughnutState = {
+          labels: ["Out of Stock", "InStock"],
+          datasets: [
+            {
+              backgroundColor: ["#00A6B4", "#6800B4"],
+              hoverBackgroundColor: ["#4B5000", "#35014F"],
+              data: [outOfStock, products.length - outOfStock],
+            },
+          ],
+        };
+      
+        return (
+          <div className="dashboard">
+            <MetaData title="Dashboard - Admin Panel" />
+            <Sidebar />
+      
+            <div className="dashboardContainer">
+              <Typography component="h1">Dashboard</Typography>
+      
+              <div className="dashboardSummary">
+                <div>
+                  <p>
+                    Total Amount <br /> ₹{totalAmount}
+                  </p>
+                </div>
+                <div className="dashboardSummaryBox2">
+                  <Link to="/admin/products">
+                    <p>Product</p>
+                    <p>{products && products.length}</p>
+                  </Link>
+                  <Link to="/admin/orders">
+                    <p>Orders</p>
+                    <p>{orders && orders.length}</p>
+                  </Link>
+                  <Link to="/admin/users">
+                    <p>Users</p>
+                    <p>{users && users.length}</p>
+                  </Link>
+                </div>
+              </div>
+      
+              <div className="lineChart">
+                <Line data={lineState} />
+              </div>
+      
+              <div className="doughnutChart">
+                <Doughnut data={doughnutState} />
+              </div>
+            </div>
+          </div>
+        );
       };
-
-     const doughnutState = {
-    labels: ["Out of Stock", "InStock"],
-    datasets: [
-      {
-        backgroundColor: ["#00A6B4", "#6800B4"],
-        hoverBackgroundColor: ["#4B5000", "#35014F"],
-        data: [outOfStock, products.length - outOfStock],
-      },
-    ],
-  };
-
-    return (
-       <>
-       {loading ?
-       <Loading />
-       :(
-        <div className="dashboard">
-        <MetaData title="Dashboard" />
-        <Sidebar />
-  
-        <div className="dashboardContainer">
-          <Typography component="h1">Dashboard</Typography>
-  
-          <div className="dashboardSummary">
-            <div>
-              <p>
-                Total Amount <br /> ${totalAmount}
-              </p>
-            </div>
-            <div className="dashboardSummaryBox2">
-              <Link to="/admin/products">
-                <p>Product</p>
-                <p>{products && products.length}</p>
-              </Link>
-              <Link to="/admin/orders">
-                <p>Orders</p>
-                <p>{orders && orders.length}</p>
-              </Link>
-              <Link to="/admin/users">
-                <p>Users</p>
-                <p>{users && users.length}</p>
-              </Link>
-            </div>
-          </div>
-  
-          <div className="lineChart">
-            <Line data={lineState} />
-          </div>
-  
-          <div className="doughnutChart">
-            <Doughnut data={doughnutState} />
-          </div>
-        </div>
-      </div>
-       )
-       }
-       </>
-    );
-  };
 export default Dashboard
